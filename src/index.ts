9bde3e757d4f3a7c37d7d30c1a7d1108b447bbff
Kieran <kieran@nitrexdesign.co.uk>
2021-05-10 00:32:32 +0100

    fix on windows

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import ora from "ora";
 import { scripts } from "../package.json";
 import fetch from "node-fetch";
 import { constants } from "fs";
-import { createReadStream } from "fs-extra";
 import { writeFile, mkdtemp, chmod, mkdir } from "fs/promises";
 import path from "path";
 import { exec, ExecException } from "child_process";
 import { tmpdir } from "os";
+import readline from "readline";
 import extract from "extract-zip";
 const minecraft_directory = require("minecraft-folder-path");
 
@@ -14,6 +14,19 @@ async function timeout(ms: number = 500) {
   return new Promise((resolve, _reject) => {
     setTimeout(resolve, ms);
   });
+}
+async function prompt(query: string) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) =>
+    rl.question(query, (ans) => {
+      rl.close();
+      resolve(ans);
+    })
+  );
 }
 
 async function child(
@@ -35,16 +48,20 @@ async function main() {
   spinner.succeed();
   spinner.text = "Downloading Forge 36.1.1 for Minecraft 1.16.5";
   // Install Minecraft Forge
+
   const forge_installer_dl = await fetch(
     `https://storage.googleapis.com/papayacraft-downloads/forge-1.16.5-36.1.1-installer.jar`
   );
   const installer = await forge_installer_dl.buffer();
   const tmp_dir = await mkdtemp(path.join(tmpdir(), "papayacraft-"));
   const forge_path = path.join(tmp_dir, "forge-install.jar");
+
   await writeFile(forge_path, installer, { encoding: "utf-8" });
-  await chmod(forge_path, constants.S_IRWXU);
+  // await chmod(forge_path, constants.S_IRWXU);
+  await chmod(forge_path, "511");
+
   spinner.text = "Installing Forge 36.1.1 for Minecraft 1.16.5";
-  const { stdout, error, stderr } = await child(`java -jar ${forge_path}`);
+  const { error } = await child(`java -jar ${forge_path}`);
   if (error !== null) {
     console.error(error);
     spinner.fail(
@@ -54,32 +71,34 @@ async function main() {
   } else {
     spinner.succeed();
   }
-  // Install Pack
   spinner.text = "Downloading Papayacraft.zip";
+
+  // Install Pack
   const install_dir = `${minecraft_directory}/papaya/v${papayacraft_version}`;
   await mkdir(install_dir, {
     recursive: true,
   });
+
   const papaya_pack_dl = await fetch(
     `https://storage.googleapis.com/papayacraft-downloads/${papayacraft_version}-papayacraft.zip`
   );
   const pack = await papaya_pack_dl.buffer();
   const pack_path = path.join(tmp_dir, "pack.zip");
+
   await writeFile(pack_path, pack);
   spinner.text = "Unzipping Papayacraft.zip";
   await timeout(500);
   await extract(pack_path, {
     dir: install_dir,
   });
+
   spinner.succeed(`Successfully Installed at ${install_dir}`);
 }
 const spinner_render_loop = setInterval(() => {
   spinner.render();
-}, 50);
+}, 66);
+
 main()
-  .then(() => {
-    console.log(`Installed successfully`);
-  })
   .catch((err) => {
     console.warn(`Install error!`);
     console.warn(err);
@@ -87,7 +106,9 @@ main()
       `please try to fix the issue above if possible and try again!`
     );
   })
-  .finally(() => {
+  .finally(async () => {
     console.log(`Wrapping up...`);
+    spinner.stop();
     clearInterval(spinner_render_loop);
+    await prompt("Press enter to exit installation! ");
   });
